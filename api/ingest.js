5a45a5ccd7fedd740c2a486396c1e6e22431c780
luketg-bit <luketgrimaldi@gmail.com>
2026-05-28 19:29:38 -0400

    Update ingest.js

--- api/ingest.js
+++ api/ingest.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const SUPABASE_URL = 'https://fasszewcztnqcjaaswcm.supabase.co';
   const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
 
